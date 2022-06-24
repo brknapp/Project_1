8a88fcd6627b6cf5f9c4279ab2ca5414cfea7f0e
Bridget Knapp <brknapp@ncsu.edu>
2022-06-23 21:12:12 -0400

    9:12_6_23_2022

--- render_code.R
+++ render_code.R
@@ -1,1 +1,6 @@
-blah
+rmarkdown::render("README.Rmd",
+                  output_format = "github_document",
+                  output_options = list(
+                    toc = TRUE,
+                    html_preview=FALSE) 
+)
