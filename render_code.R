62095dd03dca73b8758df14105257fe10a086f4d
Bridget Knapp <brknapp@ncsu.edu>
2022-06-25 16:51:03 -0400

    4:50_6_25_2022

--- render_code.R
+++ render_code.R
@@ -1,3 +1,8 @@
+#author: Bridget Knapp
+#date: 6/25/2022
+#purpose of the program: Code to create the README.md file
+###################################
+
 rmarkdown::render("README.Rmd",
                   output_format = "github_document",
                   output_options = list(
